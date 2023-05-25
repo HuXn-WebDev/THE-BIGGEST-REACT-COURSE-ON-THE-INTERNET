dc60b49095d58850d9eab3a7f4bf8015da3d9699
HuXn WebDev <webdevacademy6@gmail.com>
2023-05-25 13:07:11 -0700

    Calculator

---
+++ 3. Projects/4. Calculator/src/App.js
@@ -0,0 +1,11 @@
+import Main from "./Main";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Main />
+    </div>
+  );
+};
+
+export default App;
---
+++ 3. Projects/4. Calculator/src/Main.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import "./index.css";
+
+function Main() {
+  const [inputvalue, setinputvalue] = useState("");
+
+  function display(value) {
+    setinputvalue(inputvalue + value);
+  }
+
+  function calculate() {
+    var answers = eval(inputvalue);
+    setinputvalue(answers);
+  }
+
+  function clear() {
+    setinputvalue("");
+  }
+
+  return (
+    <form class="calculator" name="calc">
+      <input type="text" class="value" value={inputvalue} />
+      <span class="num clear" onClick={() => clear()}>
+        c
+      </span>
+      <span onClick={() => display("/")}>/</span>
+      <span onClick={() => display("*")}>*</span>
+      <span onClick={() => display("7")}>7</span>
+      <span onClick={() => display("8")}>8</span>
+      <span onClick={() => display("9")}>9</span>
+      <span onClick={() => display("-")}>-</span>
+      <span onClick={() => display("4")}>4</span>
+      <span onClick={() => display("5")}>5</span>
+      <span onClick={() => display("6")}>6</span>
+      <span className="plus" onClick={() => display("+")}>
+        +
+      </span>
+      <span onClick={() => display("1")}>1</span>
+      <span onClick={() => display("2")}>2</span>
+      <span onClick={() => display("3")}>3</span>
+      <span onClick={() => display("0")}>0</span>
+      <span onClick={() => display("00")}>00</span>
+      <span onClick={() => display(".")}>.</span>
+      <span class="num equal" onClick={() => calculate()}>
+        =
+      </span>
+    </form>
+  );
+}
+
+export default Main;
