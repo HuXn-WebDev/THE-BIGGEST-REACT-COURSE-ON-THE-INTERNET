f4f02b0b82f0bcdf94065f9900fec6934a78586a
HuXn WebDev <webdevacademy6@gmail.com>
2024-10-03 04:00:25 -0700

    Accordions

---
+++ 3. Beginners Projects/8. Accordions/utils/content.js
@@ -0,0 +1,18 @@
+export const accordionData = [
+  {
+    title: "What Is HTML?",
+    content: `The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript.`,
+  },
+  {
+    title: "What Is React?",
+    content: `React is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta and a community of individual developers and companies.`,
+  },
+  {
+    title: "What Is Node.js?",
+    content: `Node.js is a cross-platform, open-source server environment that can run on Windows, Linux, Unix, macOS, and more. Node.js is a back-end JavaScript runtime environment, runs on the V8 JavaScript Engine, and executes JavaScript code outside a web browser.`,
+  },
+  {
+    title: "What Is Golang?",
+    content: `Go is a statically typed, compiled programming language designed at Google by Robert Griesemer, Rob Pike, and Ken Thompson. It is syntactically similar to C, but with memory safety, garbage collection, structural typing, and CSP-style concurrency`,
+  },
+];
