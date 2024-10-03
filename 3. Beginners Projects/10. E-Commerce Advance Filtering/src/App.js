6cef278b23d4e342e0bebcec145c6785b22ddc1c
HuXn WebDev <webdevacademy6@gmail.com>
2024-10-03 04:01:01 -0700

    E-Commerce Advance Filtering

---
+++ 3. Beginners Projects/10. E-Commerce Advance Filtering/src/App.js
@@ -0,0 +1,82 @@
+import { useState } from "react";
+
+import Navigation from "./Navigation/Nav";
+import Products from "./Products/Products";
+import products from "./db/data";
+import Recommended from "./Recommended/Recommended";
+import Sidebar from "./Sidebar/Sidebar";
+import Card from "./components/Card";
+import "./index.css";
+
+function App() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  // ----------- Input Filter -----------
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const filteredItems = products.filter(
+    (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
+
+  // ----------- Radio Filtering -----------
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  // ------------ Button Filtering -----------
+  const handleClick = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  function filteredData(products, selected, query) {
+    let filteredProducts = products;
+
+    // Filtering Input Items
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    // Applying selected filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+
+    return filteredProducts.map(
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
+    );
+  }
+
+  const result = filteredData(products, selectedCategory, query);
+
+  return (
+    <>
+      <Sidebar handleChange={handleChange} />
+      <Navigation query={query} handleInputChange={handleInputChange} />
+      <Recommended handleClick={handleClick} />
+      <Products result={result} />
+    </>
+  );
+}
+
+export default App;
---
+++ 3. Beginners Projects/10. E-Commerce Advance Filtering/src/Sidebar/Colors/Colors.js
@@ -0,0 +1,65 @@
+import "./Colors.css";
+import Input from "../../components/Input";
+
+const Colors = ({ handleChange }) => {
+  return (
+    <>
+      <div>
+        <h2 className="sidebar-title color-title">Colors</h2>
+        <label className="sidebar-label-container">
+          <input onChange={handleChange} type="radio" value="" name="test1" />
+          <span className="checkmark all"></span>
+          All
+        </label>
+
+        <Input
+          handleChange={handleChange}
+          value="black"
+          title="Black"
+          name="test1"
+          color="black"
+        />
+
+        <Input
+          handleChange={handleChange}
+          value="blue"
+          title="Blue"
+          name="test1"
+          color="blue"
+        />
+
+        <Input
+          handleChange={handleChange}
+          value="red"
+          title="Red"
+          name="test1"
+          color="red"
+        />
+
+        <Input
+          handleChange={handleChange}
+          value="green"
+          title="Green"
+          name="test1"
+          color="green"
+        />
+
+        <label className="sidebar-label-container">
+          <input
+            onChange={handleChange}
+            type="radio"
+            value="white"
+            name="test1"
+          />
+          <span
+            className="checkmark"
+            style={{ background: "white", border: "2px solid black" }}
+          ></span>
+          White
+        </label>
+      </div>
+    </>
+  );
+};
+
+export default Colors;
