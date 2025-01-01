29a79784f9a121be2e4a4d78bb2fb992dbd083a3
HuXn WebDev <webdevacademy6@gmail.com>
2025-01-01 03:34:42 -0800

    Rendering Simple Button To Storybook

---
+++ 16. Storybook/1. Rendering Simple Button/components/Button.tsx
@@ -0,0 +1,5 @@
+const Button = () => {
+  return <button>Click Me</button>;
+};
+
+export default Button;
---
+++ 16. Storybook/1. Rendering Simple Button/stories/Button.stories.tsx
@@ -0,0 +1,8 @@
+import Button from "../components/Button";
+
+export default {
+  title: "Button",
+  component: Button,
+};
+
+export const Primary = () => <Button />;
